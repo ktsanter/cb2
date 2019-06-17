26557af27bcc6c549e08edb63bd97109009a7966
SpunkyAardvark123 <ktsanter@gmail.com>
2019-06-17 09:35:24 -0400

    added initialization with stored save comment text

--- scripts/require_savecomment.js
+++ scripts/require_savecomment.js
@@ -2,6 +2,7 @@ define(function (require) {
   require('savecomment');
   require('create_element');
   require('markdowntohtml');
+  require('standard_notice');
   
   document.addEventListener('DOMContentLoaded', app.init());
 });
